58834933ef2a3ec22aa46212aa17e2d8adab1f8c
Gagan Kaul <63837949+gagankaul@users.noreply.github.com>
2022-03-06 17:50:52 -0500

    updated

--- basketball-roster.py
+++ basketball-roster.py
@@ -1,6 +1,16 @@
 # Basketball Roster App
 
-# This is a test to be deleted later. I am checking if I can push a file from my desktop on
-# to the Internet using git.
+print("Welcome to the Basketball Roster Program\n")
 
-# Testing
+roster = {}
+
+roster["Point Guard"] = input("Who is your point guard: ").title()
+roster["Shooting Guard"] = input("Who is your shooting guard: ").title()
+roster["Small Forward"] = input("Who is your small forward: ").title()
+roster["Power Forward"] = input("Who is your power forward: ").title()
+roster["Center"] = input("Who is your center: ").title()
+
+print(f"\n\tYour starting 5 for the upcoming Basketball Season")
+for key,value in roster.items():
+  print(f"\t\t{key}:\t\t{value}".expandtabs(4))
+
